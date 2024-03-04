5198f3f7ca72287fede12b060cf712ec1329ef07
Richard T <74796181+rtillies@users.noreply.github.com>
2024-03-04 15:43:05 -0500

    Add event listener, test API call

--- script.js
+++ script.js
@@ -5,3 +5,23 @@ const searchText = document.querySelector('#search-field')
 const dropdown = document.querySelector('#dropdown')
 const accordion = document.querySelector('#accordionExample')
 
+searchButton.addEventListener ('click', searchTerm)
+
+async function searchTerm(event) {
+  const options = {
+    method: "GET",
+    url: "https://mashape-community-urban-dictionary.p.rapidapi.com/define",
+    params: { term: "fly" },
+    headers: {
+      "X-RapidAPI-Key": "750a17b4fcmsh7cef245e5bdf231p15b39ejsn3613a3a94360",
+      "X-RapidAPI-Host": "mashape-community-urban-dictionary.p.rapidapi.com",
+    },
+  };
+
+  try {
+    const response = await axios.request(options);
+    console.log(response.data);
+  } catch (error) {
+    console.error(error);
+  }
+}
