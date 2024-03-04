3aea339eb48d23e347fc437751d00afd46fe3aa7
Richard T <74796181+rtillies@users.noreply.github.com>
2024-03-04 15:42:36 -0500

    Get DOM elements

---
+++ script.js
@@ -0,0 +1,7 @@
+// import axios from 'axios';
+
+const searchButton = document.querySelector('#search-button')
+const searchText = document.querySelector('#search-field')
+const dropdown = document.querySelector('#dropdown')
+const accordion = document.querySelector('#accordionExample')
+
