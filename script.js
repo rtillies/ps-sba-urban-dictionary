891c118e4595aba324a8b15b9ad9ce1c50d83a6f
Richard T <74796181+rtillies@users.noreply.github.com>
2024-03-07 15:07:13 -0500

    Cleanup

--- script.js
+++ script.js
@@ -2,8 +2,6 @@
 
 const searchTerms = ['fly', 'phat', 'dope', 'fresh', 'def', 'bling', 'props', 'word', 'ill', 'homie', 'crib']
 
-// const searchButton = document.querySelector('#search-button')
-// const searchText = document.querySelector('#search-field')
 const dropdown = document.querySelector('#dropdown')
 const resetAllButton = document.querySelector('#reset-all')
 const accordion = document.querySelector('#accordionExample')
@@ -31,9 +29,7 @@ const populateDropdown = () => {
 }
 populateDropdown()
 clearAccordion()
-// createAccordionItem("i", 3)
 
-// searchButton.addEventListener ('click', searchTerm)
 dropdown.addEventListener ('change', searchTerm)
 resetAllButton.addEventListener ('click', resetAll)
 
@@ -79,7 +75,6 @@ function clearAccordion() {
 }
 
 function changeTopItem(item) {
-  // const topItem = document.getElementById('top-item')
   console.log("Top Item", topItem);
   const topButton = topItem.getElementsByTagName('button')[0]
   console.log("Top Button", topButton);
@@ -93,52 +88,18 @@ function changeTopItem(item) {
     </a>
     </span>
     `
-  // `<b>${item.word}</b>`
 
   const topBody = topItem.getElementsByTagName('div')[0]
   console.log("Top Body", topBody);
   topBody.innerHTML = ''
-  // topBody.innerHTML = 
-  // `<p>
-  //   Permalink: 
-  //   <a href="${item.permalink}" 
-  //     target="_blank" 
-  //     alt="Permalink to definition">
-  //     ${item.word}
-  //   </a></p>
-  // `
   wordHeading.innerText = item.word;
 }
 
 async function createAccordionItem(item, index) {
-  // const testItem = {
-  //   "definition": "[sexy] or [hella fine]",
-  //   "permalink": "http://fly.urbanup.com/11451940",
-  //   "thumbs_up": 48,
-  //   "author": "reger3785",
-  //   "word": "fly",
-  //   "defid": 11451940,
-  //   "current_vote": "",
-  //   "written_on": "2017-04-19T00:09:36.591Z",
-  //   "example": "[damn girl] you fly",
-  //   "thumbs_down": 3  
-  // }
-
-  // console.log(item.word);
-  // console.log(item.written_on);
-  // console.log(item.definition);
-  // console.log(item.permalink);
-  // console.log(item.example);
   let percent = Math.round(item.thumbs_up * 100 / (item.thumbs_up + item.thumbs_down))
   const date = item.written_on.split('T')[0]
-
-  // console.log(`${percent}%`);
   let minValue = Number(minApproval.innerText)
-  // console.log(percent, minValue);
-  if (percent < minValue) {
-    // console.log(`Don't post ${date}`);
-    return
-  } 
+  if (percent < minValue) return
 
   const accordionItem = document.createElement('div')
   accordionItem.classList.add('accordion-item')
@@ -165,7 +126,6 @@ async function createAccordionItem(item, index) {
   const accordionBody = document.createElement('div')
   accordionBody.classList.add('accordion-body')
 
-  // const date = item.written_on.split('T')[0]
   const cleanDefinition = await removeProfanity(item.definition)
   const cleanExample = await removeProfanity(item.example)
   accordionButton.innerHTML = `
@@ -182,21 +142,6 @@ async function createAccordionItem(item, index) {
       ${cleanExample}
     </p>
     `
-    // <p>
-    //   <i class="fa-solid fa-thumbs-up"></i> ${percent}%
-    // </p>
-  // accordionBody.innerHTML = `
-  //   <p><b>Definition</b><br />
-  //     ${item.definition}
-  //     </p>
-  //     <p><b>Example</b><br />
-  //     ${item.example}
-  //   </p>
-  //   <p>
-  //     <i class="fa-solid fa-thumbs-up"></i> ${percent}%
-  //   </p>
-  //   `
-
   accordionHeader.append(accordionButton)
   accordionCollapse.append(accordionBody)
   accordionItem.append(accordionHeader)
@@ -219,7 +164,6 @@ async function removeProfanity(text) {
   
   try {
     const response = await axios.request(options);
-    // console.log("Clean data", response.data);
     console.log("Clean result", response.data.result);
     return response.data.result
   } catch (error) {
@@ -249,5 +193,4 @@ function resetPlaceholder() {
   p.innerText = "Get definitions and examples for your search term here!"
   div.append(p)
   topBody.append(div)
-  
 }
